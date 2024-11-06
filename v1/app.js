c62cf77fb9f47a87b625a8a679b0d8cd61c1c52b
jeffrey <jeffrey.fanjf@gmail.com>
2024-11-06 15:13:28 -0800

    update links

--- v1/app.js
+++ v1/app.js
@@ -3,65 +3,72 @@ import { V } from "./util.mjs";
 
 import * as THREE from "three";
 
-
 export default class App extends util.Target {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        window.app = this;
+    window.app = this;
 
-        this.addHandler("start", () => {
-            let id = setInterval(async () => {
-                if (document.readyState != "complete") return;
-                this.setup();
-                clearInterval(id);
-                let t0 = null;
-                const update = async () => {
-                    window.requestAnimationFrame(update);
-                    let t1 = util.getTime();
-                    if (t0 == null) return t0 = t1;
-                    this.update(t1-t0);
-                    t0 = t1;
-                };
-                update();
-            }, 10);
-        });
+    this.addHandler("start", () => {
+      let id = setInterval(async () => {
+        if (document.readyState != "complete") return;
+        this.setup();
+        clearInterval(id);
+        let t0 = null;
+        const update = async () => {
+          window.requestAnimationFrame(update);
+          let t1 = util.getTime();
+          if (t0 == null) return (t0 = t1);
+          this.update(t1 - t0);
+          t0 = t1;
+        };
+        update();
+      }, 10);
+    });
 
-        this.addHandler("setup", () => {
-            const eWindow = document.getElementById("window");
-            if (eWindow) {
-                const tFlicker = 500;
-                const tBloom = 500;
-                const tStart = util.getTime();
-                let valm = null, val = null, valt = 0;
-                this.addHandler("update", delta => {
-                    let t = util.getTime()-tStart;
-                    if (t < tFlicker) {
-                        if (valm != 0) {
-                            valm = 0;
-                            val = null;
-                            valt = 0;
-                        }
-                        if (valt <= 0) {
-                            val = Math.random();
-                            valt += util.lerp(10, 50, Math.random());
-                        } else valt -= delta;
-                        eWindow.style.setProperty("--r", util.lerp(0, 0.5, val));
-                        return;
-                    }
-                    t -= tFlicker;
-                    if (t < tBloom) {
-                        eWindow.style.setProperty("--r", util.lerp(0.5, 1, t/tBloom));
-                        return;
-                    }
-                });
+    this.addHandler("setup", () => {
+      const eWindow = document.getElementById("window");
+      if (eWindow) {
+        const tFlicker = 500;
+        const tBloom = 500;
+        const tStart = util.getTime();
+        let valm = null,
+          val = null,
+          valt = 0;
+        this.addHandler("update", (delta) => {
+          let t = util.getTime() - tStart;
+          if (t < tFlicker) {
+            if (valm != 0) {
+              valm = 0;
+              val = null;
+              valt = 0;
             }
+            if (valt <= 0) {
+              val = Math.random();
+              valt += util.lerp(10, 50, Math.random());
+            } else valt -= delta;
+            eWindow.style.setProperty("--r", util.lerp(0, 0.5, val));
+            return;
+          }
+          t -= tFlicker;
+          if (t < tBloom) {
+            eWindow.style.setProperty("--r", util.lerp(0.5, 1, t / tBloom));
+            return;
+          }
         });
-    }
+      }
+    });
+  }
 
-    start() { this.post("start"); }
+  start() {
+    this.post("start");
+  }
 
-    setup() { this.post("setup"); }
+  setup() {
+    this.post("setup");
+  }
 
-    update(delta) { this.post("update", delta); }
+  update(delta) {
+    this.post("update", delta);
+  }
 }
